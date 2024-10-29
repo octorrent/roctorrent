53d8d76d6211baa0674e9c0bda8e312e962984fc
DrBrad <brad@bradeagle.com>
2024-10-29 01:29:33 -0600

    Glade

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-
 use std::process::exit;
-use gtk::prelude::*; // Import all necessary traits from gtk::prelude
-use gtk::{CssProvider, Image, Orientation, Window, WindowType};
+use gtk::prelude::*; // Import all necessary traits from gtk3::prelude
+use gtk::{Builder, CssProvider, Image, Orientation, Window, WindowType};
 
 /*
 sudo apt update
@@ -17,10 +16,16 @@ sudo apt install -y libgtk-3-dev
 fn main() {
     gtk::init().expect("Failed to initialize GTK.");
 
-    let window = Window::new(WindowType::Toplevel);
+    let builder = Builder::from_file("res/ui/gtk3/window.ui");
+
+    // Get the window and other widgets from the builder
+    let window: Window = builder
+        .object("main_window")  // Use the ID you set in Glade for the main window
+        .expect("Failed to get the main_window from layout.ui");
+
+
+    //let window = Window::new(WindowType::Toplevel);
     window.set_title("OcTorrent");
-    window.set_default_size(1280, 720);
-    window.set_resizable(true);
     window.connect_destroy(|_| exit(0));
 
     window.show_all();
