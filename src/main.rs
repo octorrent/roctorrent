eb73443c237903377c9b1259e997cafd2f2e6d1a
DrBrad <brad@bradeagle.com>
2024-10-29 18:23:21 -0600

    Styling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+use std::os::linux::raw::stat;
 use std::process::exit;
 use gtk::prelude::*; // Import all necessary traits from gtk3::prelude
-use gtk::{Application, Builder, Window, WindowType, Box as GtkBox, gio, SeparatorMenuItem, Menu};
+use gtk::{Application, Builder, Window, WindowType, Box as GtkBox, gio, SeparatorMenuItem, Menu, CssProvider, StyleContext, gdk};
 
 fn main() {
     let app = Application::new(Some("com.octorrent.rust"), Default::default());
 
     app.connect_activate(|app| {
         let builder = Builder::from_file("res/ui/gtk3/window.ui");
+
+        let provider = CssProvider::new();
+        provider.load_from_path("res/ui/gtk3/style.css").expect("Failed to load CSS file.");
+
+        StyleContext::add_provider_for_screen(
+            &gdk::Screen::default().expect("Failed to get default screen."),
+            &provider,
+            gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+        );
 
         // Get the window and other widgets from the builder
         let window: Window = builder
@@ -18,6 +28,15 @@ fn main() {
         //let window = Window::new(WindowType::Toplevel);
         window.set_title("OcTorrent");
         window.connect_destroy(|_| exit(0));
+
+
+        let statusbar: GtkBox = builder
+            .object("statusbar")
+            .expect("Couldn't find 'main_window_menu' in menu.ui");
+        statusbar.set_widget_name("statusbar");
+
+
+
 
         let builder = Builder::from_file("res/ui/octorrent-ui.xml");
         let menubar: gio::Menu = builder
